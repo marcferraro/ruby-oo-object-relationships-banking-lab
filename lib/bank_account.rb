e2e989eaeb1f7db7d181d1fdbcaf151d0e532ab4
Marc Ferraro <marc.ferraro1@gmail.com>
2020-12-11 20:19:04 -0600

    commit tree 510741b4777faadafec943cfc66cc36e03ea8fb4

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
+require 'pry'
 class BankAccount
 
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    def initialize(name)
+        #binding.pry
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(money)
+        @balance += (money)
+    end
+
+    def display_balance
+        return "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+       balance > 0 && status == "open"
+    end
+    
+    def close_account
+        self.status= "closed"
+    end
 end
+bank = BankAccount.new("Bank")
+bank.balance = 0
+#binding.pry
